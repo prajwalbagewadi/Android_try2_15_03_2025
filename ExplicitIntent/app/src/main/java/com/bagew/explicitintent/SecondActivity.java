0d790aee6bf684d07922909dc0448d3423132655
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-03-16 18:08:35 +0530

    Explicit Intent

---
+++ ExplicitIntent/app/src/main/java/com/bagew/explicitintent/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.bagew.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d("lifeCycle", "SecondActivity onCreate()");
+    }
+}
