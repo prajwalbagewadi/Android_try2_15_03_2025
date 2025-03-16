6f129700302d0fd854390d4a366b25a52fbcaaf6
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-03-16 22:57:35 +0530

    Implicit Intent with call and search

---
+++ ImplicitIntent/app/src/main/java/com/bagew/implicitintent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.bagew.implicitintent;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.call_dad);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:+919823205922"));
+            startActivity(intent);
+        });
+
+        Button btn2 = findViewById(R.id.google);
+        btn2.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.google.com"));
+            startActivity(intent);
+        });
+
+    }
+}
